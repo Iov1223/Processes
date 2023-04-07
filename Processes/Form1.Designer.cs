4f6e7c1ddbf65a0debb32690dc37e20c6c9e22e9
andre <andrej_1987@bk.ru>
2023-04-08 00:33:49 +0500

    Добавьте файлы проекта.

---
+++ Processes/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Processes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.BackgroundImage = global::Processes.Properties.Resources.calculator_icon_icons_com_72046;
+            this.buttonCalc.Location = new System.Drawing.Point(13, 29);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(50, 51);
+            this.buttonCalc.TabIndex = 0;
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCalc;
+    }
+}
+
---
+++ Processes/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Processes
+{
+    public partial class Form1 : Form
+    {
+        private Process proc = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (proc != null && proc.HasExited)
+                {
+                    proc = null;
+                    MessageBox.Show("Калькулятор уже запущен");
+                }
+                else
+                {
+                    proc = Process.Start("calc");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
+
